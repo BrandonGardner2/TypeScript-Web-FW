ca2d6d45928403591a248c6f5a72a509e00341ae
Brandon Gardner <brandon.gardner2@outlook.com>
2020-01-02 16:23:55 -0600

    converts sync to a generic

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,3 @@
-import axios, { AxiosResponse } from 'axios';
-
 import { Eventing } from './Eventing';
 
 export interface UserProps {
@@ -19,30 +17,5 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  fetch(): void {
-    axios.get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((res: AxiosResponse): void => {
-        this.set(res.data);
-      })
-      .catch((err: Error) => console.log(err))
-  }
-
-  save(): void {
-    const id = this.get('id');
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data)
-        .then((res: AxiosResponse): void => {
-          this.set(res.data);
-        })
-        .catch((err: Error) => console.log(err));
-    } else {
-      axios.post(`http://localhost:3000/users`, this.data)
-      .then((res: AxiosResponse): void => {
-        this.set(res.data);
-      })
-      .catch((err: Error) => console.log(err));
-    }
   }
 }
