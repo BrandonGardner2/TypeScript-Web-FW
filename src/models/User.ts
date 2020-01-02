04c476c471e78f6a18a2bb26d7cd927d3298e33b
Brandon Gardner <brandon.gardner2@outlook.com>
2020-01-02 17:51:10 -0600

    implements set and fetch for user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,7 @@
 import { Attributes } from './Attributes';
 import { Sync } from './Sync';
 import { Eventing } from './Eventing';
+import { AxiosResponse } from 'axios';
 
 export interface UserProps {
   id?: number;
@@ -27,5 +28,25 @@ export class User {
 
   get get() {
     return this.attributes.get;
+  }
+
+  set(update: UserProps): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.get('id');
+
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id.');
+    }
+
+    this.sync.fetch(id)
+      .then((res: AxiosResponse): void => {
+        const { data } = res;
+        this.set(data);
+      })
+      .catch((err: Error) => console.log(err));
   }
 }
